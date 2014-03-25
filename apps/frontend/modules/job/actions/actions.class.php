247f481cb8097518b7b1e0e974674c5953725ff6
Daniel Alvarenga Lima <danielalvarengalima@gmail.com>
2014-03-25 18:59:04 -0300

    update template show job

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -16,8 +16,8 @@ class jobActions extends sfActions
 
   public function executeShow(sfWebRequest $request)
   {
-    $this->JobeetJob = JobeetJobPeer::retrieveByPk($request->getParameter('id'));
-    $this->forward404Unless($this->JobeetJob);
+    $this->job = JobeetJobPeer::retrieveByPk($request->getParameter('id'));
+    $this->forward404Unless($this->job);
   }
 
   public function executeNew(sfWebRequest $request)
